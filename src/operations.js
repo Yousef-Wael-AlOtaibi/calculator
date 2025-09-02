be32b4745ebe47dcc70456ae89ccfb0a557e1d30
Yousef-Wael-AlOtaibi <ywalotaibi2010@gmail.com>
2025-09-02 19:56:13 +0300

    decimal problem fix, and wider calculator

--- src/operations.js
+++ src/operations.js
@@ -1,5 +1,5 @@
-export const add = (a,b)=>(a+b).toFixed(2);
-export const subtract = (a,b)=>(a-b).toFixed(2);
-export const multiply = (a,b)=>(a*b).toFixed(2);
-export const divide = (a,b)=>(a/b).toFixed(2);
+export const add = (a,b)=>{return (a+b) % 1 === 0? a+b : (a+b).toFixed(2)};
+export const subtract = (a,b)=>{return (a-b) % 1 === 0? a-b : (a-b).toFixed(2)};
+export const multiply = (a,b)=>{return (a*b) % 1 === 0? a*b : (a*b).toFixed(2)};
+export const divide = (a,b)=>{return (a/b) % 1 === 0? a/b : (a/b).toFixed(2)};
 export const opposite = (number) => -number;
